8162c64a7d02b0dc93d1433f78259c0e4101f621
Роман Слободенюк <slobodenyuk@emportal.ru>
2022-05-11 20:55:43 +0300

    download code

--- code.js
+++ code.js
@@ -9,8 +9,8 @@ function traverse(node,categoryBox, cardBox) {
 
     if (node.type == 'FRAME' && node.name != "Template - DON'T FILL IN JUST COPY") {
       textNode = node.children[0]
-      
-    if (Math.round(node.fills[0].color.r*255) == parseInt(String(categoryBox).slice(1,3),16)
+
+      if (Math.round(node.fills[0].color.r*255) == parseInt(String(categoryBox).slice(1,3),16)
         && Math.round(node.fills[0].color.g*255) == parseInt(String(categoryBox).slice(3,5),16) 
         && Math.round(node.fills[0].color.b*255) == parseInt(String(categoryBox).slice(5,7),16)) {
         dir = ""
@@ -58,13 +58,20 @@ function sorter () {
   newCount = titel.slice(0,-1) + "\n" + newCount
 }
 
-function outputer () {
-  textNode = figma.createText()
-  textNode.fontName = { family: "Work Sans", style: "Regular" }
-  textNode.characters = newCount
+function outputerAsync () {
+  return new Promise(resolve => {
+    textNode = figma.createText()
+    textNode.fontName = { family: "Work Sans", style: "Regular" }
+    textNode.characters = newCount
+    
+    figma.ui.postMessage({ type: 'download', data: newCount })
+    setTimeout(() => {
+      resolve();
+    }, 600);
+  })
 }
 
-const loadFonts = async () => {
+const loadFontsAsync = async () => {
   await figma.loadFontAsync({ family: "Work Sans", style: "Regular" })
   await figma.loadFontAsync({ family: "Work Sans", style: "Medium" })
   await figma.loadFontAsync({ family: "Work Sans", style: "Bold" })
@@ -75,13 +82,14 @@ if (figma.editorType === 'figma') {
     figma.ui.onmessage = msg => {
         if (msg.type === 'create-shapes') {
             
-            traverse(figma.root, msg.categoryBox, msg.cardBox) 
+            traverse(figma.root, msg.categoryBox, msg.cardBox)
 
-            loadFonts().then(() => {
-            sorter()
-            outputer()
-            newCount = ""
-            figma.closePlugin()
+            loadFontsAsync().then(() => {
+              sorter()
+              outputerAsync().then(() => {
+                newCount = ""
+                figma.closePlugin()
+              })
             })
 
         }
